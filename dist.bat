ed2212ce8fbaf6818437f7350f9ae4c209f7c307
ZCube <zcube@zcube.kr>
2019-05-30 16:18:27 +0900

    update dist.bat

--- dist.bat
+++ dist.bat
@@ -2,12 +2,12 @@ git pull
 pip3 install filelock pylzma
 pushd bin\Windows\x64_QT5.12.3\Release
 git init
-git add -f *
+git add *
 git commit -m "Commit"
 popd
 pushd bin\Windows\x86_QT5.12.3\Release
 git init
-git add -f *
+git add *
 git commit -m "Commit"
 popd
 py -3 patch_gen.py bin\Windows\x64_QT5.12.3\Release dist OverlayProc_x64_QT5.12.3
